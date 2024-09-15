0dad9e1450b4fb23ff383695a6270a45ac2bfc60
Firmansyah <147714843+Manzzzx@users.noreply.github.com>
2024-09-15 21:34:35 +0700

    init

---
+++ 02_Days_Tipe_Data/string_methods/accessing_character.js
@@ -0,0 +1,12 @@
+// Akses karakter pertama di string 'JavaScript'.
+
+let string = 'JavaScript'
+let hurufPertama = string[0]
+console.log(hurufPertama) // J
+let hurufKedua = string[1] // a
+let hurufKetiga = string[2]
+let hurufTerakhir = string[9]
+console.log(hurufTerakhir) // t
+let indeksTerakhir = string.length - 1
+console.log(indeksTerakhir) // 9
+console.log(string[indeksTerakhir]) // t
